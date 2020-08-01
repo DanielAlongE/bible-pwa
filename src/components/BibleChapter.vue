3500c7f260e0a87e0085210ce116b3f5d90dd4fe
Daniel Alonge <BrownMarble@outlook.com>
2020-08-01 01:40:28 +0100

    add BibleChapter comp

---
+++ src/components/BibleChapter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bookChapter">
+    <h1>{{ msg }}</h1>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class BibleChapter extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <BibleChapter msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import BibleChapter from "@/components/BibleChapter.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    BibleChapter
   }
 };
 </script>
