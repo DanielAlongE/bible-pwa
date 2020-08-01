42ddd0f245ca7410e5d5cd065bb7aaba718e68e0
Daniel Alonge <BrownMarble@outlook.com>
2020-08-01 04:28:27 +0100

    add shared/bibleData

--- src/components/BibleChapter.vue
+++ src/components/BibleChapter.vue
@@ -1,16 +1,81 @@
 <template>
   <div class="bookChapter">
     <h1>{{ msg }}</h1>
-
+    <div>
+      <select v-model="bookId">
+        <option v-for="book in books" :key="book.index" :value="book.index">{{book.name}}</option>
+      </select>
+      <select v-model="chapterId">
+        <option v-for="chapter in chapters" :key="chapter" :value="chapter">{{chapter}}</option>
+      </select>
+      <button @click="goToChapter">Go</button>
+    </div>
+    <p v-for="verse in verses" :key="verse.id">{{verse.v}} {{verse.text}}</p>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import { getChapter, addVerses } from "@/shared/idbService.ts";
+import bibleData, { BibleData } from "@/shared/bibleData.ts";
 
 @Component
 export default class BibleChapter extends Vue {
   @Prop() private msg!: string;
+
+  books: BibleData[] = [];
+  verses: any[] = [];
+
+  get chapters(){
+    const count = this.books[this.bookId].chapters;
+    console.log(count, this.chapterId)
+    if(count < this.chapterId){
+      this.chapterId = 1;
+    }
+    return Array(count).fill(null).map((_,i) => i+1)
+  }
+
+  translation: string = "kjv";
+  bookId: number = 0;
+  chapterId: number = 1;
+  verseId: number = 1;
+
+  created(){
+    console.log("Yaay I have been created!");
+
+    this.initBooks();
+
+    getChapter("kjv-0-1").then(res => this.verses = res);
+
+    //this.populateDB();
+  }
+
+  goToChapter(){
+    const { translation, bookId, chapterId } = this;
+    const key = `${translation}-${bookId}-${chapterId}`;
+    getChapter(key).then(res => this.verses = res);
+  }
+
+  initBooks(){
+    this.books = [ ...bibleData ];
+  }
+
+
+  populateDB(){
+    fetch("http://127.0.0.1:5500/app/kjv.json")
+      .then( r => r.json())
+      .then( (data: any[]) => {
+        
+        const nData = data.map(x => {
+          const uuid = `${x.cId}-${x.v}`
+          return ({...x, uuid})
+        })
+
+        console.log(nData)
+        //addVerses(nData, "verses")
+      })
+  }
+
 }
 </script>
 
--- src/shared/idbService.ts
+++ src/shared/idbService.ts
@@ -1,7 +1,7 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const BIBLE_DB = "bible";
-const VERSES_TABLE = "kjv";
+const VERSES_TABLE = "verses";
 const VERSION = 1;
 
 export async function connnectDB () {
@@ -13,6 +13,7 @@ export async function connnectDB () {
                     //verses.createIndex('uuid', 'uuid', { unique: true })
     
                     const kjv = db.createObjectStore(VERSES_TABLE, {keyPath: 'id', autoIncrement: true });
+                    kjv.createIndex('uuid', 'uuid', { unique: true })
                     kjv.createIndex('bId', 'bId');
                     kjv.createIndex('cId', 'cId');                
       
@@ -23,14 +24,14 @@ export async function connnectDB () {
 }
 
 
-export async function getChapter(key: string) {
+export async function getChapter(key: string): Promise<any[]> {
 
     try {
 
       const db = await connnectDB();
 
-        var tx = db.transaction(VERSES_TABLE, 'readonly');
-        var store = tx.objectStore(VERSES_TABLE);
+        const tx = db.transaction(VERSES_TABLE, 'readonly');
+        const store = tx.objectStore(VERSES_TABLE);
         const index = store.index('cId');
         let cursor =  await index.openCursor( IDBKeyRange.bound(key, key) );
 
@@ -44,11 +45,45 @@ export async function getChapter(key: string) {
         cursor = await cursor.continue();
 
       }
-
+      console.log(result);
       return result;
 
     } catch (error) {
       console.log(error);
       return [];
     }
+  }
+
+  export async function addVerses(items:any[], table:string ) {
+
+    const db = await connnectDB();
+
+      let promisesArray = []
+
+      const tx = db.transaction(table, 'readwrite');
+      const store = tx.objectStore(table);
+
+
+      promisesArray = items.map(function(item) {
+
+        return new Promise(resolve => {
+          console.log('Adding item: ', item);
+            resolve(store.add(item));
+        }).catch(err=> err)
+
+        });
+      
+      return Promise.all( promisesArray ).then(function(res) {
+          if(res){
+            console.log('All items added successfully!');
+            return res;          
+          }
+
+      }).catch(function(e) {
+        tx.abort();
+        console.log('an error occured', e);
+      }).finally(function(){
+
+      });
+
   }
