a5a4878557edcb78fc030310cdf55e1c8c810fd8
Daniel Alonge <BrownMarble@outlook.com>
2020-08-10 04:10:43 +0100

    add shims-chakra.d.ts

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import Chakra, { CThemeProvider, CReset } from '@chakra-ui/vue'
 
 Vue.config.productionTip = false;
 
+Vue.use(Chakra)
+
 new Vue({
   router,
-  render: h => h(App)
+  //render: h => h(App)
+  render: (h) => h(CThemeProvider, [h(CReset), h(App)])
 }).$mount("#app");
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,17 +2,25 @@
   <div class="about">
     <h1>Import Bible translation</h1>
     <BibleImport />
+    <c-box>
+      <c-button>
+        Chakra Consumed! ⚡️
+      </c-button>
+    </c-box>
   </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
 import BibleImport from "@/components/BibleImport.vue";
-
+import { CBox, CButton } from '@chakra-ui/vue'
 export default {
   name: "Import",
   components: {
-    BibleImport
+    BibleImport,
+    CBox,
+    CButton
   }
 };
 </script>
