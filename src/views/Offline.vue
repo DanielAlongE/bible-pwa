475606be099be19048d982dec2f5921eb6aae3dc
Daniel Alonge <BrownMarble@outlook.com>
2020-08-11 13:03:06 +0100

    add service-worker

---
+++ src/views/Offline.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <c-box>
+      <c-text>
+        Opps you seem to be offline
+      </c-text>
+    </c-box>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { CText, CBox } from '@chakra-ui/vue'
+
+export default {
+  name: "Import",
+  components: {
+    CBox,
+    CText,
+  }
+};
+</script>
