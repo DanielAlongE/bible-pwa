8b5ed1e04c7c50f98cb28eb7add2d28f551c53f9
Daniel Alonge <BrownMarble@outlook.com>
2020-10-05 13:34:29 +0100

    add DrawerMenu

---
+++ src/components/DrawerMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2 v-if="isOpen">DrawerMenu</h2>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    isOpen: Boolean,
+    onClose: Function
+  },
+  data: function() {
+    return {};
+  }
+});
+</script>
