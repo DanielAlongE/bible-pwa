6e82ff3abe30eca5a0a87e87d94e529eb7538733
Daniel Alonge <BrownMarble@outlook.com>
2020-10-10 11:36:30 +0100

    add views/Settings.vue

---
+++ src/views/Settings.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <BibleImport />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BibleImport from "@/components/BibleImport.vue";
+
+export default {
+  name: "Import",
+  components: {
+    BibleImport
+  }
+};
+</script>
