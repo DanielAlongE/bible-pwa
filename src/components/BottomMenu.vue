f58d46ea2c01447b417d58fe19896cef1edadefd
Daniel Alonge <BrownMarble@outlook.com>
2020-10-12 11:54:07 +0100

    update BottomMenu add history

--- src/components/BottomMenu.vue
+++ src/components/BottomMenu.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="bottom-menu" id="nav">
-    <c-flex
-      rounded="lg"
-      bg="gray.200"
-      h="50px"
-      w="100px"
-      align="center"
-      justify="space-around"
-    >
-      <router-link to="/">
-        <c-icon name="home" size="14px" />
-      </router-link>
-      <router-link to="/about">
-        <c-icon name="info-circle" size="14px" />
-      </router-link>
-      <router-link to="/settings">
-        <c-icon name="cog" size="14px" />
-      </router-link>
+    <c-flex justify="space-between">
+      <c-flex
+        rounded="lg"
+        bg="gray.200"
+        h="50px"
+        w="100px"
+        align="center"
+        justify="space-around"
+      >
+        <router-link to="/">
+          <c-icon name="home" size="20px" />
+        </router-link>
+        <router-link to="/history">
+          <c-icon name="history" size="20px" />
+        </router-link>
+      </c-flex>
+
+      <c-flex
+        rounded="lg"
+        bg="gray.200"
+        h="50px"
+        w="100px"
+        align="center"
+        justify="space-around"
+      >
+        <router-link to="/about">
+          <c-icon name="info-circle" size="20px" />
+        </router-link>
+        <router-link to="/settings">
+          <c-icon name="cog" size="20px" />
+        </router-link>
+      </c-flex>
     </c-flex>
   </div>
 </template>
@@ -42,6 +57,5 @@ export default class BottomMenu extends Vue {
 .bottom-menu {
   position: fixed;
   bottom: 5px;
-  right: 5px;
 }
 </style>
