d2a80771df557030f9aadc9c97b825cd199282f3
Daniel Alonge <BrownMarble@outlook.com>
2020-10-13 12:52:37 +0100

    update shared/history

--- src/shared/history.ts
+++ src/shared/history.ts
@@ -1,3 +1,27 @@
-export const getHistoryList = () => {
-  
+type HistoryList = [string, number, number]
+
+const item_key = '_history';
+
+function save(history: HistoryList[]){
+  localStorage.setItem(item_key, JSON.stringify(history))
+}
+
+export const get = (): HistoryList[] => {
+  try {
+    const h = localStorage.getItem(item_key);
+    if(h){
+      return JSON.parse(h)
+    }
+    throw new Error("Parse Error");
+    
+  } catch (error) {
+    console.log(error)
+    return []
+  }
+}
+
+export const push = (h: HistoryList) => {
+  let history = get()
+  history.push(h)
+  save(history)
 }
