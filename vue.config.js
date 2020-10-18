2dfaa0c086849d943a69c0379df275ac272e6da2
Daniel Alonge <BrownMarble@outlook.com>
2020-10-18 23:45:15 +0100

    update vue.config add manifestOption

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/camelcase */
 module.exports = {
   lintOnSave: true,
   pwa: {
@@ -14,6 +15,14 @@ module.exports = {
       // swSrc is required in InjectManifest mode.
       swSrc: "src/service-worker.js",
       exclude: [/\.map$/, /manifest\.json$/]
+    },
+    manifestOptions: {
+      name: "Bible Roni",
+      short_name: "Bible",
+      display: "standalone",
+      theme_color: "#260751",
+      start_url: ".",
+      background_color: "#ffffff"
     }
   }
 };
